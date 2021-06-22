8c53db67e9436dea52b20700cb04409c4590457e
Hyung June Kwak <kwakdent95@gmail.com>
2021-06-22 00:29:27 -0700

    Security dependecies added_210622

---
+++ src/main/java/com/cmpt276/gameinn/GameinnApplication.java
@@ -0,0 +1,13 @@
+package com.cmpt276.gameinn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameinnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameinnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cmpt276/gameinn/GameinnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cmpt276.gameinn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameinnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
