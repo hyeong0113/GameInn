cd911cb03d50a1ebea0a354527813f68a255677b
Hyung June Kwak <kwakdent95@gmail.com>
2021-07-04 15:46:52 -0700

    fetching profile after login is added_2107041546

--- src/main/java/com/cmpt276/gameinn/controllers/HomeController.java
+++ src/main/java/com/cmpt276/gameinn/controllers/HomeController.java
@@ -10,6 +10,9 @@ import org.springframework.ui.Model;
 public class HomeController {
     @GetMapping("/")
     public String home(Model model, @AuthenticationPrincipal OidcUser principal) {
+        if (principal != null) {
+            model.addAttribute("profile", principal.getClaims());
+        }
         return "index";
     }
 }
