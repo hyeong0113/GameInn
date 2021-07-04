272a61b4d41fda3b12fa03198b61fcb05def36dc
Hyung June Kwak <kwakdent95@gmail.com>
2021-07-03 21:44:30 -0700

    first commit after configuration_2107032144

---
+++ src/main/java/com/cmpt276/gameinn/controllers/HomeController.java
@@ -0,0 +1,15 @@
+package com.cmpt276.gameinn.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.ui.Model;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String getHome(Model model, @AuthenticationPrincipal OidcUser principal) {
+        return "index";
+    }
+}
