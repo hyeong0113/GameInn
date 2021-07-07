3cb50a1cf29c3c626482b2fee6da76e41fdd2f01
Hyung June Kwak <kwakdent95@gmail.com>
2021-07-07 16:40:58 -0700

    Working on profile UI

--- src/main/java/com/cmpt276/gameinn/controllers/HomeController.java
+++ src/main/java/com/cmpt276/gameinn/controllers/HomeController.java
@@ -3,6 +3,7 @@ package com.cmpt276.gameinn.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.ui.Model;
@@ -22,6 +23,7 @@ public class HomeController {
     @GetMapping("/")
     public String home(@AuthenticationPrincipal OidcUser principal, Model model) {
         if (principal != null) {
+            System.out.println(principal.getClaims());
             String sub = principal.getClaims().get("sub").toString();
             String name = principal.getClaims().get("name").toString();
             String email = principal.getClaims().get("email").toString();
