2ac38fcc1405d7f5cf47d54ecaee14dcac16823d
Christopher Louie <cla311@sfu.ca>
2021-08-03 19:19:57 -0700

    Add social accounts to User entity and profile

--- src/main/java/com/cmpt276/gameinn/controllers/GroupFinderController.java
+++ src/main/java/com/cmpt276/gameinn/controllers/GroupFinderController.java
@@ -2,107 +2,129 @@ package com.cmpt276.gameinn.controllers;
 
 import javax.validation.Valid;
 
+import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import javax.servlet.http.HttpServletRequest;
 
+import com.cmpt276.gameinn.auth.HandleCookie;
 import com.cmpt276.gameinn.models.GroupFinder;
 import com.cmpt276.gameinn.models.User;
 import com.cmpt276.gameinn.services.*;
-import com.cmpt276.gameinn.auth.HandleCookie;
 
 @Controller public class GroupFinderController {
-    @Autowired private GroupFinderService groupFinderService;
-    @Autowired private UserService userService;
+	@Autowired private GroupFinderService groupFinderService;
+	@Autowired private UserService userService;
 
-	@GetMapping(value = {"/groupfinders", "/groupfinders/{sub}"}) public String groupFinderListPage(@PathVariable(required = false)String sub,
-                                                                    Model model, HttpServletRequest request, @AuthenticationPrincipal OidcUser principal) {
-        
-        String url="";
-        if (principal != null) {
-            String role = userService.getRoleFromResponse(principal);
-            if (!role.equals("admin") && HandleCookie.readCookie(request, HandleCookie.COOKIE_NAME) != sub) {
-                model.addAttribute("show", false);
-            }
-            else {
-                model.addAttribute("show", true);
-            }
-            model.addAttribute("user", userService.getUserBySub(HandleCookie.readCookie(request, HandleCookie.COOKIE_NAME)));
-            url=String.format("/groupfinders/%s/addEdit", HandleCookie.readCookie(request, HandleCookie.COOKIE_NAME));
-        }
-        else {
-            url="/oauth2/authorization/auth0";
-        }
-        model.addAttribute("url", url);
-        model.addAttribute("groupFinders", groupFinderService.getGroupFinders());
+	@GetMapping(value = { "/groupfinders", "/groupfinders/{sub}" }) public
+	String groupFinderListPage(@PathVariable(required = false) String sub, Model
+		model, HttpServletRequest request, @AuthenticationPrincipal OidcUser
+		principal) {
+		String url = "";
 
-        return "groupFinderList";
-    }
+		if (principal != null) {
+			String role = userService.getRoleFromResponse(principal);
 
-    @GetMapping("/groupfinders/{sub}/addEdit")
-    public String showAddEditGroupFinderPageForCreate(@PathVariable(required = true)String sub, GroupFinder groupFinder, Model model, HttpServletRequest request) {
-		model.addAttribute("user", userService.getUserBySub(HandleCookie.readCookie(request, HandleCookie.COOKIE_NAME)));
-        return "addEditGroupFinderPage";
-    }
+			if (!role.equals("admin") && HandleCookie.readCookie(request,
+				HandleCookie.COOKIE_NAME) != sub)
+				model.addAttribute("show", false);
+			else
+				model.addAttribute("show", true);
 
-    // Assure User is logged in and have an authorization to create
-    @PostMapping("/groupfinders/{sub}/addEdit/add") public String addGroupFinder(@PathVariable(required = true)String sub,
-                                                                                @Valid GroupFinder groupFinder, BindingResult result,
-                                                                                Model model, HttpServletRequest request) {
-        model.addAttribute("user", userService.getUserBySub(HandleCookie.readCookie(request, HandleCookie.COOKIE_NAME)));
-        if (result.hasErrors()) {
-            return "addEditGroupFinderPage";
-        }
+			model.addAttribute("user", userService.getUserBySub(
+				HandleCookie.readCookie(request, HandleCookie.COOKIE_NAME)));
+			url = String.format("/groupfinders/%s/addEdit",
+				HandleCookie.readCookie(request, HandleCookie.COOKIE_NAME));
+		} else {
+			url = "/oauth2/authorization/auth0";
+		}
 
-        User user = userService.getUserBySub(sub);
-        GroupFinder temp = groupFinderService.addGroupFinder(groupFinder, user);
+		model.addAttribute("url", url);
+		model.addAttribute("groupFinders",
+			groupFinderService.getGroupFinders());
+
+		return "groupFinderList";
+	}
+
+	@GetMapping("/groupfinders/{sub}/addEdit") public
+	String showAddEditGroupFinderPageForCreate(@PathVariable(required =
+		true) String sub, GroupFinder groupFinder, Model model,
+		HttpServletRequest request) {
+		model.addAttribute("user", userService.getUserBySub(
+			HandleCookie.readCookie(request, HandleCookie.COOKIE_NAME)));
+		return "addEditGroupFinderPage";
+	}
+
+	// Assure User is logged in and have an authorization to create
+	@PostMapping("/groupfinders/{sub}/addEdit/add") public
+	String addGroupFinder(@PathVariable(required = true) String sub,
+		@Valid GroupFinder groupFinder, BindingResult result, Model model,
+		HttpServletRequest request) {
+		model.addAttribute("user", userService.getUserBySub(
+			HandleCookie.readCookie(request, HandleCookie.COOKIE_NAME)));
+
+		if (result.hasErrors())
+			return "addEditGroupFinderPage";
+
+		User user = userService.getUserBySub(sub);
+		GroupFinder temp = groupFinderService.addGroupFinder(groupFinder, user);
 
 		return "redirect:/groupfinders/" + sub;
 	}
 
-    @GetMapping(value = {"/groupfinders/detail/{id}", "/groupfinders/{sub}/detail/{id}"}) public String getGroupFinderById(@PathVariable(required = false) String sub,
-                                                                                                        @PathVariable Long id, Model model, HttpServletRequest request) {
-        model.addAttribute("user", userService.getUserBySub(HandleCookie.readCookie(request, HandleCookie.COOKIE_NAME)));
-        model.addAttribute("groupFinder", groupFinderService.getGroupFinderByID(id));
-        return "groupFinderDetail";
-    }
+	@GetMapping(value = {
+		"/groupfinders/detail/{id}", "/groupfinders/{sub}/detail/{id}"
+	}) public String getGroupFinderById(@PathVariable(required = false) String
+		sub, @PathVariable Long id, Model model, HttpServletRequest request) {
+		model.addAttribute("user", userService.getUserBySub(
+			HandleCookie.readCookie(request, HandleCookie.COOKIE_NAME)));
+		model.addAttribute("groupFinder", groupFinderService.getGroupFinderByID(
+			id));
+		return "groupFinderDetail";
+	}
 
-    @GetMapping("/groupfinders/{sub}/addEdit/{id}")
-    public String showAddEditGroupFinderPageForEdit(@PathVariable(required = true)String sub, @PathVariable Long id,
-                                                                                GroupFinder groupFinder, Model model, HttpServletRequest request) {
-        model.addAttribute("user", userService.getUserBySub(HandleCookie.readCookie(request, HandleCookie.COOKIE_NAME)));
-        model.addAttribute("groupFinder", groupFinderService.getGroupFinderByID(id));
-        return "addEditGroupFinderPage";
-    }
+	@GetMapping("/groupfinders/{sub}/addEdit/{id}") public
+	String showAddEditGroupFinderPageForEdit(@PathVariable(required =
+		true) String sub, @PathVariable Long id, GroupFinder groupFinder, Model
+		model, HttpServletRequest request) {
+		model.addAttribute("user", userService.getUserBySub(
+			HandleCookie.readCookie(request, HandleCookie.COOKIE_NAME)));
+		model.addAttribute("groupFinder", groupFinderService.getGroupFinderByID(
+			id));
+		return "addEditGroupFinderPage";
+	}
 
-    @RequestMapping("/groupfinders/{sub}/addEdit/edit/{id}") public String editGroupFinder(@PathVariable(required = true)String sub, @PathVariable Long id,
-                                                                                @Valid GroupFinder groupFinder, BindingResult result,
-                                                                                Model model, HttpServletRequest request) throws Exception {
-        model.addAttribute("user", userService.getUserBySub(HandleCookie.readCookie(request, HandleCookie.COOKIE_NAME)));
-        if (result.hasErrors()) {
-            return "addEditGroupFinderPage";
-        }
+	@RequestMapping("/groupfinders/{sub}/addEdit/edit/{id}") public
+	String editGroupFinder(@PathVariable(required = true) String sub,
+		@PathVariable Long id, @Valid GroupFinder groupFinder, BindingResult
+		result, Model model, HttpServletRequest request)
+	throws Exception {
+		model.addAttribute("user", userService.getUserBySub(
+			HandleCookie.readCookie(request, HandleCookie.COOKIE_NAME)));
 
-        GroupFinder temp = groupFinderService.updateGroupFinder(id, groupFinder);
+		if (result.hasErrors())
+			return "addEditGroupFinderPage";
+
+		GroupFinder temp = groupFinderService.updateGroupFinder(id,
+			groupFinder);
 
 		return "redirect:/groupfinders/" + sub;
 	}
 
-    @RequestMapping("/groupfinders/{sub}/delete/{id}")
-    public String deleteGroupFinder(@PathVariable(required = true)String sub, @PathVariable Long id, Model model, HttpServletRequest request, @AuthenticationPrincipal OidcUser principal) {
-        model.addAttribute("user", userService.getUserBySub(HandleCookie.readCookie(request, HandleCookie.COOKIE_NAME)));
-        groupFinderService.deleteGroupFinder(id);
-        return "redirect:/groupfinders/" + sub;
-    }
-
-
- }
-
+	@RequestMapping("/groupfinders/{sub}/delete/{id}") public
+	String deleteGroupFinder(@PathVariable(required = true) String sub,
+		@PathVariable Long id, Model model, HttpServletRequest request,
+		@AuthenticationPrincipal OidcUser principal) {
+		model.addAttribute("user", userService.getUserBySub(
+			HandleCookie.readCookie(request, HandleCookie.COOKIE_NAME)));
+		groupFinderService.deleteGroupFinder(id);
+		return "redirect:/groupfinders/" + sub;
+	}
+}
--- src/main/java/com/cmpt276/gameinn/models/Clip.java
+++ src/main/java/com/cmpt276/gameinn/models/Clip.java
@@ -4,10 +4,10 @@ import java.util.Date;
 import java.util.List;
 import java.util.Objects;
 
+import javax.persistence.*;
 import javax.persistence.Column;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
-import javax.persistence.*;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
@@ -21,47 +21,38 @@ import org.hibernate.annotations.OnDeleteAction;
 
 import javax.persistence.Table;
 
-@Entity @Table(name = "clips")
-public class Clip {
-    @Id @GeneratedValue private Long id;
+@Entity @Table(name = "clips") public class Clip {
+	@Id @GeneratedValue private Long id;
 
-    @NotNull
-    private String title;
+	@NotNull private String title;
 
-    @NotNull
-    private String gameTitle;
+	@NotNull private String gameTitle;
 
-    @ElementCollection @Column(name = "tag_list")
-    private List<String> tags;
+	@ElementCollection @Column(name = "tag_list") private List<String> tags;
 
-    @NotNull
-    private Platform platform;
+	@NotNull private Platform platform;
 
-    @NotNull
-    private String sourceLink;
+	@NotNull private String sourceLink;
 
-    @NotNull
-    private Date postedTime;
+	@NotNull private Date postedTime;
 
-    @ManyToOne
-    @JoinColumn(name="user_id")
-    @OnDelete(action = OnDeleteAction.CASCADE)
-    private User RUser;
+	@ManyToOne @JoinColumn(name = "user_id") @OnDelete(action =
+	OnDeleteAction.CASCADE) private User RUser;
 
-    public Clip() {}
+	public Clip() {}
 
-    public Clip(String title, String gameTitle, List<String> tags, Platform platform, String sourceLink, Date postedTime, User user) {
-        this.title = title;
-        this.gameTitle = gameTitle;
-        this.tags = tags;
-        this.platform = platform;
-        this.sourceLink = sourceLink;
-        this.postedTime = postedTime;
-        this.RUser = user;
-    }
+	public Clip(String title, String gameTitle, List<String> tags, Platform
+		platform, String sourceLink, Date postedTime, User user) {
+		this.title = title;
+		this.gameTitle = gameTitle;
+		this.tags = tags;
+		this.platform = platform;
+		this.sourceLink = sourceLink;
+		this.postedTime = postedTime;
+		this.RUser = user;
+	}
 
-
-    public Long getId() {
+	public Long getId() {
 		return this.id;
 	}
 
@@ -73,9 +64,9 @@ public class Clip {
 		return this.title;
 	}
 
-    public void setTitle(String title){
-        this.title = title;
-    }
+	public void setTitle(String title) {
+		this.title = title;
+	}
 
 	public String getGameTitle() {
 		return this.gameTitle;
@@ -85,63 +76,55 @@ public class Clip {
 		this.gameTitle = gameTitle;
 	}
 
-    public List<String> getTags()
-    {
-        return this.tags;
-    }
+	public List<String> getTags() {
+		return this.tags;
+	}
 
-    public void setTags(List<String> tags)
-    {
-        this.tags = tags;
-    }
+	public void setTags(List<String> tags) {
+		this.tags = tags;
+	}
 
-    public Platform getPlatform()
-    {
-        return this.platform;
-    }
+	public Platform getPlatform() {
+		return this.platform;
+	}
 
-    public void setPlatform(Platform platform)
-    {
-        this.platform = platform;
-    }
-    
-    public String getSourceLink()
-    {
-        return this.sourceLink;
-    }
+	public void setPlatform(Platform platform) {
+		this.platform = platform;
+	}
 
-    public void setSourceLink(String sourceLink)
-    {
-        this.sourceLink = sourceLink;
-    }
+	public String getSourceLink() {
+		return this.sourceLink;
+	}
 
-    public Date getPostedTime()
-    {
-        return this.postedTime;
-    }
+	public void setSourceLink(String sourceLink) {
+		this.sourceLink = sourceLink;
+	}
 
-    public void setPostedTime(Date postedTime)
-    {
-        this.postedTime = postedTime;
-    }
+	public Date getPostedTime() {
+		return this.postedTime;
+	}
 
-    public User getUser()
-    {
-        return this.RUser;
-    }
+	public void setPostedTime(Date postedTime) {
+		this.postedTime = postedTime;
+	}
 
-    public void setUser(User user)
-    {
-        this.RUser = user;
-    }
+	public User getUser() {
+		return this.RUser;
+	}
 
-    @Override public int hashCode() {
-		return Objects.hash(this.title, this.gameTitle, this.tags, this.sourceLink, this.postedTime);
+	public void setUser(User user) {
+		this.RUser = user;
+	}
+
+	@Override public int hashCode() {
+		return Objects.hash(this.title, this.gameTitle, this.tags,
+			this.sourceLink, this.postedTime);
 	}
 
 	@Override public String toString() {
-		return "Employee{" + "title=" + this.title + ", gametiitle='" + this.gameTitle +
-			 ", tags='" + this.tags + ", platform='" + this.platform.toString() + ", sourceLink='" + this.sourceLink + ", postedTime='" +
-			   this.postedTime + '\'' + '}';
+		return "Employee{" + "title=" + this.title + ", gametiitle='" +
+			   this.gameTitle + ", tags='" + this.tags + ", platform='" +
+			   this.platform.toString() + ", sourceLink='" + this.sourceLink +
+			   ", postedTime='" + this.postedTime + '\'' + '}';
 	}
 }
