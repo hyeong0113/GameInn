7b091566d891bcc314d18ea7d94e715dd76efe2a
Hyung June Kwak <kwakdent95@gmail.com>
2021-08-03 20:13:40 -0700

    Clip list descending order and show time difference_2108032013

--- src/main/resources/static/js/clip_list.js
+++ src/main/resources/static/js/clip_list.js
@@ -6,3 +6,30 @@ function getId(url) {
         match[2] :
         null;
 }
+
+function display_diff_time(posted_time) {
+    var seconds = Math.floor((new Date() - new Date(posted_time)) / 1000);
+
+    var interval = seconds / 31536000;
+
+    if (interval > 1) {
+        return Math.floor(interval) + " years ago";
+    }
+    interval = seconds / 2592000;
+    if (interval > 1) {
+        return Math.floor(interval) + " months ago";
+    }
+    interval = seconds / 86400;
+    if (interval > 1) {
+        return Math.floor(interval) + " days ago";
+    }
+    interval = seconds / 3600;
+    if (interval > 1) {
+        return Math.floor(interval) + " hours ago";
+    }
+    interval = seconds / 60;
+    if (interval > 1) {
+        return Math.floor(interval) + " minutes ago";
+    }
+    return "few seconds ago";
+}
