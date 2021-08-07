1e1c9e25dbfe103b91393b7a19a05051067d3575
Christopher Louie <cla311@sfu.ca>
2021-08-06 21:01:35 -0700

    Switch to minified files

--- src/main/resources/static/js/add_edit_clip.js
+++
@@ -1,7 +0,0 @@
-function set_action_url(clip) {
-    if (clip.getId() === null) {
-        return "@{/clips/{sub}/addEdit/add(sub=${user.getSubId()})}";
-    } else {
-        return "@{/clips/{sub}/addEdit/edit/{id}(sub=${clip.getUser().getSubId()}, id=${clip.getId()})}";
-    }
-}
--- src/main/resources/static/js/profile.js
+++ src/main/resources/static/js/profile.js
@@ -1,34 +1,6 @@
 $(function() {
     $("textarea").height($("textarea")[0].scrollHeight);
-
-    hideEditButtons();
-
-    $("#editProfile").click(function() {
-        showEditButtons();
-    });
-
-    $("#cancel").click(function() {
-        hideEditButtons();
-    });
 });
-
-function showEditButtons() {
-    $("#editProfile").hide();
-    $("#saveProfile").show();
-    $("#cancel").show();
-
-    $(".social").hide();
-    $(".socialInput").show();
-}
-
-function hideEditButtons() {
-    $("#editProfile").show();
-    $("#saveProfile").hide();
-    $("#cancel").hide();
-
-    $(".social").show();
-    $(".socialInput").hide();
-}
 
 function load_admin_tag(role) {
     if (role == "admin") {
